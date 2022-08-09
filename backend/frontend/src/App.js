9111f9b0e75b9feb8cdbe3e2ee7435c1e44aa314
Сергей <sergey@csb-mirena.ru>
2022-08-09 14:34:17 +0300

    Lesson 2 (#2)
    lesson_2

---
+++ backend/frontend/src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import MenuList from './components/Menu.js'
+import UserList from './components/Users.js'
+import axios from 'axios'
+
+class App extends React.Component {
+  menu = [
+    {
+      'name': 'Главная',
+      'url': '/'
+    },
+    {
+      'name': 'Контакты',
+      'url': '/contact'
+    },
+  ]
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      'menu': this.menu,
+      'users': []
+    }
+  }    
+
+  componentDidMount() {    
+    axios.get('http://127.0.0.1:8000/api/user')
+    .then(response => {
+      const users = response.data
+      this.setState(
+        {
+          'users': users,          
+        }
+      )
+    }).catch(error => console.log(error))    
+  }
+
+  render () {
+    return (      
+      <div>
+        <MenuList list_menu={this.state.menu} />  
+        <div className="content">
+          <UserList users={this.state.users} />  
+        </div>          
+      </div>
+    )
+  }
+}
+export default App;
---
+++ backend/frontend/src/components/Users.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const UserItem = ({user}) => {
+    return (
+        <tr>
+            <td>
+                {user.username}
+            </td>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+            <td>
+                {user.email}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({users}) => {
+    return (
+        <table>
+            <th>
+                Nik
+            </th>
+            <th>
+                Имя
+            </th>
+            <th>
+                Фамилия
+            </th>
+            <th>
+                Email
+            </th>
+            {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList
